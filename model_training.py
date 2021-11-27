4e04c9e068a35e12fcb65bb70248e2b1d7ba0b84
Kirti1807 <LCI2020039@iiitl.ac.in>
2021-11-27 15:18:36 +0530

    Model Training and Evaluating completed
    I trained model with XgBoost and calculated accuracy

--- model_training.py
+++ model_training.py
@@ -13,8 +13,8 @@ from sklearn.ensemble import  RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import RandomizedSearchCV
 
-# import xgboost as xgb
-# from xgboost import XGBClassifier
+import xgboost as xgb
+from xgboost import XGBClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
 class ModelTraining:
@@ -23,6 +23,7 @@ class ModelTraining:
         self.x_train = x_train
         self.y_train = y_train
 
+    # Logistic Regression Model
     def logistic(self):
         print("Begining training of model for LogisticRegression")
         log_reg = LogisticRegression()
@@ -30,7 +31,54 @@ class ModelTraining:
         print("Completed training for LogisticRegression")
         return log_reg    
 
-        
+    # XgBoost Model
+    def Xgboost_model(self, fine_tuning=True):
+        if fine_tuning:
+            print("Started Finetuning the model:- ")
+            n_estimators = [50, 100, 150, 200]
+            max_depth = [2, 4, 6, 8]
+            learning_rate = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3]
+            subsample = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0]
+            colsample_bytree = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0]
+            min_child_weight = [1, 2, 3, 4, 5]
+            gamma = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+            params = {
+                "n_estimators": n_estimators,
+                "max_depth": max_depth,
+                "learning_rate": learning_rate,
+                "subsample": subsample,
+                "colsample_bytree": colsample_bytree,
+                "min_child_weight": min_child_weight,
+                "gamma": gamma,
+            }
+            xgb = XGBClassifier()
+            clf = RandomizedSearchCV(xgb, params, cv=5, n_jobs=-1)
+            clf.fit(self.x_train, self.y_train)
+            print("Finished Hyperparameter search")
+            return clf
+        else:
+            print("Started training the model:- ")
+            xgb = XGBClassifier(
+                learning_rate=0.3,
+                max_delta_step=0,
+                max_depth=8,
+                min_child_weight=1,
+                n_estimators=50,
+                n_jobs=16,
+                num_parallel_tree=1,
+                random_state=0,
+                reg_alpha=0,
+                reg_lambda=1,
+                scale_pos_weight=1,
+                subsample=1.0,
+                tree_method="exact",
+                validate_parameters=1,
+                verbosity=None,
+            )
+            xgb.fit(self.x_train, self.y_train)
+            print("Completed training the model")
+            return xgb
+
 
     # model for svm
     def svm(self, fine_tune = True):
@@ -47,12 +95,19 @@ class ModelTraining:
             clf = RandomizedSearchCV(svm,params,n_jobs=-1,cv=5)
             clf.fit(self.x_train, self.y_train)
             print("Completed the hyperparameter tunning")
+            return clf
         else:
             print("starting to train the model")
-            svm = SVC(C=1, kernel='rbf', degree=3, gamma='auto', coef0=0, shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1, decision_function_shape='ovr', break_ties=False, random_state=42)
+            svm = SVC(C=1, kernel='rbf', degree=3, gamma='auto', coef0=0.0, 
+                    shrinking=True, probability=False, tol=0.001, cache_size=200,
+                    class_weight=None, verbose=False, max_iter=-1, decision_function_shape='ovr', 
+                    break_ties=False, random_state=None)
+
             svm.fit(self.x_train, self.y_train)
             print("Trainign completed")
             return svm
+
+        # Naive bayes model    
     def Naive_bayes(self):
         print("Training Naive Bayes")
         nb = MultinomialNB()
